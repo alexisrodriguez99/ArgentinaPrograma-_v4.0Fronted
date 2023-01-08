61c96e9591a86fd77a76ca20070373d626931a2a
Rodriguez Ossa Alexis Kevin <alexisrodriguezossa99@gmail.com>
2023-01-08 18:54:45 -0300

    testeo del nav

--- js/funcionesCV.js
+++ js/funcionesCV.js
@@ -19,9 +19,36 @@ $.ajax({
     //let json= Object.fromEntries(data.results[0].name.first);
     /*let json=JSON.stringify(data.results);
     console.log(json)*/
-    document.getElementById('nombre').innerHTML = data.results[0].name.first + " " + data.results[0].name.last;
-    document.getElementById('foto').src = data.results[0].picture.large
-  }
+    let nombreCompleto = data.results[0].name.first + " " + data.results[0].name.last;
+    let foto = data.results[0].picture.large;
+    let nacimiento = data.results[0].dob.date;
+    let pais = data.results[0].location.country;
+    let ciudad = data.results[0].location.city;
+    
+    let cel = data.results[0].cell;
+     let telfono = data.results[0].phone;
+    let mail = data.results[0].email;
+    let calle = data.results[0].location.street.name + " - " +data.results[0].location.street.number;
+    nacimiento = nacimiento.substring(0, 10) + " (año/mes/día)";
+
+    console.log(nacimiento)
+    var elms = document.querySelectorAll("[id='nombre']");
+    for(var i = 0; i < elms.length; i++) 
+    elms[i].innerHTML=nombreCompleto; // <-- whatever you need to do here.
+  
+ //    document.getElementById('nombre').innerHTML = nombreCompleto;
+//document.getElementById('nombreDiv').innerHTML = nombreCompleto;
+document.getElementById('foto').src = foto;
+document.getElementById('nacimiento').innerHTML = nacimiento;
+document.getElementById('pais').innerHTML = pais;
+document.getElementById('ciudad').innerHTML = ciudad;
+
+document.getElementById('celular').innerHTML = cel;
+document.getElementById('telefono').innerHTML = telfono;
+document.getElementById('mail').innerHTML = mail;
+document.getElementById('calle').innerHTML = calle;
+
+}
 });
 
 //funcion para ver ajustar el nav segun el ancho de la pantalla
@@ -44,12 +71,12 @@ function widthChangeCallback(myMediaQuery_Entre1400Y1900px) {
     if(myMediaQuery_Entre1400Y1900px.matches) {
       // document.getElementById("carta").classList.remove("me-5");  
       // document.querySelector("p").textContent = "no tengo.";
-      document.getElementById("carta").classList.add("me-5");
+      // document.getElementById("carta").classList.add("me-5");
       //document.querySelector("p").textContent = "si tengo+++++";
      } else {
       // document.getElementById("carta").classList.add("me-5");
       // document.querySelector("p").textContent = "si tengo+++++";
-      document.getElementById("carta").classList.remove("me-5");  
+      // document.getElementById("carta").classList.remove("me-5");  
       //document.querySelector("p").textContent = "no tengo.";
      }
   }
