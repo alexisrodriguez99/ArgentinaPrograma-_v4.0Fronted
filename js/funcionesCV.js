7e041ba78ad1c828cab371af43fb002818d60452
Rodriguez Ossa Alexis Kevin <alexisrodriguezossa99@gmail.com>
2023-01-12 14:25:29 -0300

    cv terminado

--- js/funcionesCV.js
+++ js/funcionesCV.js
@@ -1,103 +1,115 @@
-let myMediaQuery_Max1400px = window.matchMedia('(max-width: 1400px)');
-let myMediaQuery_Entre1400Y1900px = window.matchMedia('(min-width: 1400px) and (max-width: 1900px)');
-
-$(document).ready(function(){
-
-    //código a ejecutar cuando el DOM está listo para recibir instrucciones.
-    console.log("tamo")
-    //myMediaQuery_1700px.addEventListener('change', widthChangeCallback);
-  widthChangeCallback(myMediaQuery_Entre1400Y1900px);
-    widthChangeCallbackMax1400px(myMediaQuery_Max1400px);
-  });
-// 
+let clientHeight = document.getElementById('mail').clientHeight;
+const idNombre = document.querySelectorAll("[id='nombre']");
+const liNombre = idNombre[1];
+const liNacimiento = document.querySelector("#nacimiento");
+const liPais = document.querySelector("#pais");
+const liCiudad = document.querySelector("#ciudad");
+const liCelular = document.querySelector("#celular");
+const liTelefono = document.querySelector("#telefono");
+const liMail = document.querySelector("#mail");
+const liCalle = document.querySelector("#calle");
 $.ajax({
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function (data) {
-    console.log(data);
-    console.log(data.results[0]);
-    //let json= Object.fromEntries(data.results[0].name.first);
-    /*let json=JSON.stringify(data.results);
-    console.log(json)*/
+    // console.log(data);
+    // console.log(data.results[0]);
+
     let nombreCompleto = data.results[0].name.first + " " + data.results[0].name.last;
     let foto = data.results[0].picture.large;
     let nacimiento = data.results[0].dob.date;
     let pais = data.results[0].location.country;
     let ciudad = data.results[0].location.city;
-    
+
     let cel = data.results[0].cell;
-     let telfono = data.results[0].phone;
+    let telfono = data.results[0].phone;
     let mail = data.results[0].email;
-    let calle = data.results[0].location.street.name + " - " +data.results[0].location.street.number;
-    nacimiento = nacimiento.substring(0, 10) + " (año/mes/día)";
+    let calle = data.results[0].location.street.name + " - " + data.results[0].location.street.number;
+    nacimiento = nacimiento.substring(0, 10);
 
-    console.log(nacimiento)
-    var elms = document.querySelectorAll("[id='nombre']");
-    for(var i = 0; i < elms.length; i++) 
-    elms[i].innerHTML=nombreCompleto; // <-- whatever you need to do here.
-  
- //    document.getElementById('nombre').innerHTML = nombreCompleto;
-//document.getElementById('nombreDiv').innerHTML = nombreCompleto;
-document.getElementById('foto').src = foto;
-document.getElementById('nacimiento').innerHTML = nacimiento;
-document.getElementById('pais').innerHTML = pais;
-document.getElementById('ciudad').innerHTML = ciudad;
+    let fechaOrdenada = nacimiento.split("-");
+    fechaOrdenada = fechaOrdenada[2] + "-" + fechaOrdenada[1] + "-" + fechaOrdenada[0];
 
-document.getElementById('celular').innerHTML = cel;
-document.getElementById('telefono').innerHTML = telfono;
-document.getElementById('mail').innerHTML = mail;
-document.getElementById('calle').innerHTML = calle;
+    let elms = document.querySelectorAll("[id='nombre']");
+    for (var i = 0; i < idNombre.length; i++)
+      idNombre[i].innerHTML = nombreCompleto;
 
-}
+    document.getElementById('foto').src = foto;
+    document.getElementById('nacimiento').innerHTML = fechaOrdenada;
+    document.getElementById('pais').innerHTML = pais;
+    document.getElementById('ciudad').innerHTML = ciudad;
+
+    document.getElementById('celular').innerHTML = cel;
+    document.getElementById('telefono').innerHTML = telfono;
+    document.getElementById('mail').innerHTML = mail;
+    document.getElementById('calle').innerHTML = calle;
+
+  }
 });
+function scrollToTargetAdjusted(id) {
 
-//funcion para ver ajustar el nav segun el ancho de la pantalla
-// function widthChangeCallback(myMediaQuery) {
-//   console.log(myMediaQuery)
-//   if(myMediaQuery.matches) {
-//     document.getElementById("cont-nav").classList.remove("flex-column");
-//     //document.getElementById("cont-nav").classList.add
-
-//    // document.querySelector("p").textContent = "menos.";
-//    } else {
-//     document.getElementById("cont-nav").classList.add("flex-column");
-//     // document.querySelector("p").textContent = "mas+++++";
-//    }
-// }
-// myMediaQuery.addEventListener('change', widthChangeCallback);
-
-function widthChangeCallback(myMediaQuery_Entre1400Y1900px) {
-    //console.log(myMediaQuery_Entre1400Y1900px)
-    if(myMediaQuery_Entre1400Y1900px.matches) {
-      // document.getElementById("carta").classList.remove("me-5");  
-      // document.querySelector("p").textContent = "no tengo.";
-      // document.getElementById("carta").classList.add("me-5");
-      //document.querySelector("p").textContent = "si tengo+++++";
-     } else {
-      // document.getElementById("carta").classList.add("me-5");
-      // document.querySelector("p").textContent = "si tengo+++++";
-      // document.getElementById("carta").classList.remove("me-5");  
-      //document.querySelector("p").textContent = "no tengo.";
-     }
+  var element = document.getElementById(id);
+  var headerOffset = 180;
+  var elementPosition = element.getBoundingClientRect().top;
+  if (window.innerWidth < 992) {
+    headerOffset = 117;
   }
-  function widthChangeCallbackMax1400px(myMediaQuery_Max1400px) {
-    console.log(myMediaQuery_Max1400px)
-    if(myMediaQuery_Max1400px.matches) {
-      // document.getElementById("carta").classList.remove("me-5");  
-      // document.querySelector("p").textContent = "no tengo.";
-      document.getElementById("nav-row").classList.add("container-fluid");
-      //document.querySelector("p").textContent = "si tengo+++++";
-      document.getElementById("div-nav").classList.add("sticky-top");
-      document.getElementById("nav").classList.remove("sticky-top");
-
-     } else {
-      // document.getElementById("carta").classList.add("me-5");
-      // document.querySelector("p").textContent = "si tengo+++++";
-      document.getElementById("nav-row").classList.remove("container-fluid");  
-      //document.querySelector("p").textContent = "no tengo.";
-      document.getElementById("div-nav").classList.remove("sticky-top");
-      document.getElementById("nav").classList.add("sticky-top");
-     }
+  else if (window.innerWidth < 576) {
+    headerOffset = 100;
   }
-  myMediaQuery_Entre1400Y1900px.addEventListener('change', widthChangeCallback);
-  myMediaQuery_Max1400px.addEventListener('change', widthChangeCallbackMax1400px);
+  else if (window.innerWidth >= 1400) {
+    headerOffset = 0;
+  }
+
+  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth"
+  });
+}
+
+function verMas() {
+  let mostrar = document.getElementById("vermas").style.display;
+  //console.log(document.getElementById("vermas").style.display)
+  if (mostrar != "block" || mostrar == "null") {
+    document.getElementById("vermas").style.display = "block";
+    document.getElementById("btoVerMas").classList.remove("btn-outline-primary");
+    document.getElementById("btoVerMas").classList.add("btn-outline-danger");
+    document.getElementById("btoVerMas").innerHTML = "Ver menos";
+
+    let clientHeight = document.getElementById('mail').clientHeight;
+    //console.log(clientHeight + " en px")
+
+  }
+  else {
+    document.getElementById("vermas").style.display = "none";
+    document.getElementById("btoVerMas").classList.add("btn-outline-primary");
+    document.getElementById("btoVerMas").classList.remove("btn-outline-danger");
+  }
+}
+
+const observe = new ResizeObserver(enrtries => {
+  enrtries.forEach(element => {
+    //console.log(element)
+
+    const tamanio = element.contentRect.bottom + element.contentRect.top;
+    if (tamanio > 24) {
+      // console.log(tamanio)
+      document.getElementById('titulo_' + element.target.id).style.height = tamanio + "px";
+    }
+    else {
+      document.getElementById('titulo_' + element.target.id).style.height = tamanio + "px";
+
+    }
+  });
+})
+observe.observe(liNombre)
+observe.observe(liNacimiento)
+observe.observe(liPais)
+observe.observe(liCiudad)
+observe.observe(liCelular)
+observe.observe(liTelefono)
+observe.observe(liMail)
+observe.observe(liCalle)
+
